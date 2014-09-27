aa967f3df4fd8230085a96ef59c343e5627b5c05
Enrique Marroquin <kike93_@hotmail.com>
2014-09-27 16:42:11 -0500

    first push

--- Gruntfile.js
+++ Gruntfile.js
@@ -218,6 +218,7 @@ module.exports = function (grunt) {
         javascriptsDir: '<%= yeoman.app %>/scripts',
         fontsDir: '<%= yeoman.app %>/styles/fonts',
         importPath: './bower_components',
+        
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
         httpFontsPath: '/styles/fonts',
