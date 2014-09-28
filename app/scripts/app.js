288b27978fb1b6defafe9eeee5f2a9001cc932d2
Enrique Marroquin <kike93_@hotmail.com>
2014-09-28 00:59:08 -0500

    new proposal with map

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -78,7 +78,8 @@ angular
 				url: '/new_proposal',
 				views: {
 					'bodyView': {
-						templateUrl: 'views/new-proposal.html'
+						templateUrl: 'views/new-proposal.html',
+						controller: 'NewProposalCtrl'
 					}
 				}
 			})
